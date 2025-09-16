220bc5a12693c33a7450df41ff942f5ad51b02a5
rejee <rmflores@urios.edu.ph>
2025-09-16 11:45:35 +0800

    added text

--- text.py
+++ text.py
@@ -2,4 +2,4 @@ print("Hello, World!")
 print("Hello, github!")
 
 print("Added new line!")
-print("This is a new line created not in the text editor, but in GitHub!")
+print("This is a new line (to conflict) created not in the text editor, but in GitHub!")
