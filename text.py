991201a2e748fa536d7570017d4d8889ac085541
rejee <rmflores@urios.edu.ph>
2025-09-19 11:08:01 +0800

    header message
    more details.........

--- text.py
+++ text.py
@@ -3,3 +3,7 @@ print("Hello, github!")
 
 print("Added new line!")
 print("This is a new line (to conflict) created not in the text editor, but in GitHub!")
+
+
+print("Added new line!")
+print("This is a new line (to conflict) created not in the text editor, but in GitHub!")
